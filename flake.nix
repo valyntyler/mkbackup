8b87528f82ce2ffa98fde7a14f3a62587ad6fb9f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-07-14 20:27:46 +0300

    feat: update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
     ...
@@ -16,10 +17,29 @@
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            self.packages.${system}.default
             just
             nushell
             ouch
           ];
+        };
+        packages = rec {
+          default = mkbackup;
+          mkbackup = pkgs.stdenv.mkDerivation rec {
+            name = "mkbackup";
+            src = ./src;
+            buildInputs = with pkgs; [makeWrapper];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ./main.nu $out/bin/${name}
+              chmod +x $out/bin/${name}
+              wrapProgram $out/bin/${name} \
+                --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
+                nushell
+                ouch
+              ])}
+            '';
+          };
         };
       }
     );
